afd0d9b4b0335698bfb6918bff593d8cbd4f50e8
onjo <onjo@d2c7b242-0724-0410-acbd-db3348fdb001>
2007-04-02 11:17:01 +0000

    thumbnails

---
+++ www/screenshots/generate.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+exec >x-screenshot.html
+
+inheader=yes
+while read l; do
+    test $inheader = yes && echo "$l" 
+    (echo "$l" | grep -q "shots start here") && inheader=no
+done < screenshots.html
+
+
+grep shot: screenshots.html | sed 's/<!--//;s/-->//' | 
+while read tmp file comment; do
+#echo using $file with commment $comment
+    d=$(dirname $file)
+    f=$(basename $file)
+#    echo $d/$f
+    convert -resize 200  $d/$f $d/thumb-$f 
+    echo -n '<p><a href="'$d/$f'"><img src="'$d/thumb-$f'" width="'
+    identify $d/thumb-$f | cut -d" " -f 3 | cut -dx -f1 | (read a; echo -n $a)
+    echo -n '" height="' 
+    identify $d/thumb-$f | cut -d" " -f 3 | cut -dx -f2 | (read a; echo -n $a)
+    echo '" alt="'$comment'" /><br /> '$comment'</a></p>'
+
+done
+
+
+inheader=yes
+while read l; do
+    test $inheader = no && echo "$l" 
+    (echo "$l" | grep  "shots end here") && inheader=no
+done < screenshots.html
+
+mv x-screenshot.html screenshots.html
+
